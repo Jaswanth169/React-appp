e04f1268ff6250d5e5bbe2750a69a8834a4d18d6
Sabbineni Jaswanth <98733861+Jaswanth169@users.noreply.github.com>
2024-07-02 20:42:38 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://speechdia.azurewebsites.net/', // Replace with your backend API URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  build: {
+    outDir: 'build', // Specifies the output directory for production builds
+    // Remove 'dotenv' from rollupOptions.external
+    rollupOptions: {
+      external: [],
+    },
+  },
+});
